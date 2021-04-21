fff62729be94bb03ae60a52e98149a98e3e86a97
Hugo Cordeiro <hp.cordeiro@hotmail.com>
2021-04-20 23:16:34 -0300

    Implementing search async

--- src/main/java/com/github/hpchugo/es/DataEndpoint.java
+++ src/main/java/com/github/hpchugo/es/DataEndpoint.java
@@ -11,11 +11,20 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.asyncsearch.AsyncSearchResponse;
+import org.elasticsearch.client.asyncsearch.SubmitAsyncSearchRequest;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller("/data")
 public class DataEndpoint {
@@ -36,7 +45,7 @@ public class DataEndpoint {
         return source;
     }
 
-    @ExecuteOn(TaskExecutors.IO)
+
     @Get("/document/async/{id}")
     public CompletableFuture<String> byIdAsAsync(@PathVariable("id") String documentId) throws IOException {
         final CompletableFuture<String> whenDone = new CompletableFuture<>();
@@ -54,6 +63,30 @@ public class DataEndpoint {
                 whenDone.completeExceptionally(e);
             }
         });
+        return whenDone;
+    }
+
+
+    @Get("/document/async/firstname/{search}")
+    public CompletableFuture<String> byFirstNameAsAsync(@PathVariable("search") String search) throws IOException {
+        final CompletableFuture<String> whenDone = new CompletableFuture<>();
+        final SearchSourceBuilder source =  new SearchSourceBuilder()
+                .query(QueryBuilders.matchQuery("first_name", search));
+        client.asyncSearch().submitAsync(new SubmitAsyncSearchRequest(source, Constants.INDEX), RequestOptions.DEFAULT, new ActionListener<AsyncSearchResponse>() {
+            @Override
+            public void onResponse(AsyncSearchResponse asyncSearchResponse) {
+                SearchHit[] hits = asyncSearchResponse.getSearchResponse().getHits().getHits();
+                List<String> response = Stream.of(hits).map(SearchHit::getSourceAsString).collect(Collectors.toList());
+                LOG.debug("Response /document/{} () => {}", search, response);
+                whenDone.complete(response.toString());
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                whenDone.completeExceptionally(e);
+            }
+        });
+
         return whenDone;
     }
 }
